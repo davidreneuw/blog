e7f4c846798652836b485d4b189b3fca2860b495
davidreneuw <davrene11@gmail.com>
2024-07-25 23:24:29 -0400

    Topbar and login

---
+++ src/components/sidesheet.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+import { RxHamburgerMenu } from "react-icons/rx";
+export function SideSheet() {
+  return (
+    <Sheet>
+      <div className="md:hidden">
+        <SheetTrigger asChild>
+          <button className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">
+            <RxHamburgerMenu size={25} />
+          </button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Musings by Carlos</SheetTitle>
+            <SheetDescription>A blog by Carlos Delos Santos</SheetDescription>
+          </SheetHeader>
+          <nav className="flex flex-col mt-4">
+            <ClerkLoaded>
+              <SignedOut>
+                <SignInButton>
+                  <button className="text-gray-800 hover:bg-gray-100 px-3 py-2 rounded-md text-xl text-start">
+                    Sign in
+                  </button>
+                </SignInButton>
+              </SignedOut>
+              <SignedIn>
+                <div className="flex self-center mb-4">
+                  <UserButton showName />
+                </div>
+              </SignedIn>
+            </ClerkLoaded>
+            <ClerkLoading>
+              <button className="text-gray-800 hover:bg-gray-100 px-3 py-2 rounded-md text-xl text-start">
+                Sign in
+              </button>
+            </ClerkLoading>
+            <a
+              href="/"
+              className="text-gray-800 hover:bg-gray-100 px-3 py-2 rounded-md text-xl text-start"
+            >
+              Home
+            </a>
+            <a
+              href="/about"
+              className="text-gray-800 hover:bg-gray-100 px-3 py-2 rounded-md text-xl text-start"
+            >
+              About
+            </a>
+          </nav>
+        </SheetContent>
+      </div>
+    </Sheet>
+  );
+}
---
+++ src/components/topbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+import { dark } from "@clerk/themes";
+import { SideSheet } from "./sidesheet";
+
+export function Topbar() {
+  return (
+    <header className="bg-gray-800">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <a href="/" className="flex items-center">
+              <img
+                className="h-10 w-10 color-white"
+                src="/bird.svg"
+                alt="Logo"
+              />
+              <h1 className="text-white text-lg ml-2">Musings by Carlos</h1>
+            </a>
+            <nav className="hidden md:block ml-10 space-x-4">
+              <a
+                href="/"
+                className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Home
+              </a>
+              <a
+                href="/about"
+                className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                About
+              </a>
+            </nav>
+          </div>
+          <SideSheet />
+          <div className="hidden md:block">
+            <ClerkLoading>
+              <button className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">
+                Sign in
+              </button>
+            </ClerkLoading>
+            <ClerkLoaded>
+              <SignedOut>
+                <SignInButton>
+                  <button className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">
+                    Sign in
+                  </button>
+                </SignInButton>
+              </SignedOut>
+              <SignedIn>
+                <div className="text-white">
+                  <UserButton showName appearance={{ baseTheme: dark }} />
+                </div>
+              </SignedIn>
+            </ClerkLoaded>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
